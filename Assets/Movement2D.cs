e8e4c27423794b94791d9668880e49a48c71a5a4
Elias <elgu58hbg@gmail.com>
2020-11-12 20:33:06 +0100

    Lade till en placeholdersprite och Basic movement för player

---
+++ Assets/Movement2D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        var movement_h = Input.GetAxis("Horizontal");
+        var movement_v = Input.GetAxis("Vertical");
+        transform.position += new Vector3(movement_h, movement_v, 0) * Time.deltaTime * MovementSpeed;
+        
+    }
+}
