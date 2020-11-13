77b38dcf7b27f76938081ab664956293ba6d69ca
Leo <leo.hasselqvist@elev.ntig.se>
2020-11-13 13:47:01 +0100

    Uppdaterade filstrukturen, player movement och fixade collisions

---
+++ Assets/Scripts/Movement2D.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    [SerializeField]
+    private float baseSpeed = 10;
+    [SerializeField]
+    private float speedMod = 1;
+
+    public Rigidbody2D rb;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        var movement_h = Input.GetAxis("Horizontal") * baseSpeed * speedMod;
+        var movement_v = Input.GetAxis("Vertical") * baseSpeed * speedMod;
+        //transform.position += new Vector3(movement_h, movement_v, 0) * Time.deltaTime * baseSpeed * speedMod;*/
+
+        rb.velocity = new Vector2(movement_h, movement_v);
+        
+    }
+}
