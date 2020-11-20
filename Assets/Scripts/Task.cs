a42a828211d94a492641d71726994c790267244a
Leo <leo.hasselqvist@elev.ntig.se>
2020-11-20 13:32:55 +0100

    Fixade basic trigger för task

---
+++ Assets/Scripts/Task.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Task: Started");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Task: ENTER from " + other.name);
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Task: EXIT");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
