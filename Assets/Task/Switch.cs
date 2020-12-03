4060517edc244eca12a83fe08b24b02645d1f85e
Anton <anton.enell@wlwv.ntig.se>
2020-12-03 14:47:03 +0100

    Fixat scripen för att slå på/av switches

---
+++ Assets/Task/Switch.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    public GameObject up;
+    public GameObject on;
+    public bool isOn;
+    public bool isUp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        on.SetActive(isOn);
+        up.SetActive(isUp);
+
+        if (isOn)
+        {
+
+        }
+    }
+    private void OnMouseUp()
+    {
+        isUp = !isUp;
+        isOn = !isOn;
+
+        on.SetActive(isOn);
+        up.SetActive(isUp);
+    }
+}
