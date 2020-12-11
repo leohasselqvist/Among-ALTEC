4993b6f92e67ad488327f22a78386f8fecb8c766
Anton <anton.enell@wlwv.ntig.se>
2020-12-11 13:18:34 +0100

    nu kan task prefab-sen Instantiate-as

--- Assets/Scripts/Task.cs
+++ Assets/Scripts/Task.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class Task : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public GameObject taskPrefab; //pls link me to my task, om inte funkar testa public GameObject taskPrefab = null; 
+
+
     void Start()
     {
+
+       
         Debug.Log("Task: Started");
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other) // om man går nära tasken länka collider
     {
+        
         Debug.Log("Task: ENTER from " + other.name);
         
     }
 
-    private void OnTriggerExit2D(Collider2D other)
+    private void OnTriggerExit2D(Collider2D other) // om man lämnar collidern stängs tasken
     {
         Debug.Log("Task: EXIT");
     }
@@ -25,7 +32,13 @@ public class Task : MonoBehaviour
     public void Popup()
     {
         Debug.Log("Task: POPUP!");
+
+        GameObject.Instantiate(taskPrefab);
+
+        Instantiate(taskPrefab, new Vector3(0, 0, -10), Quaternion.identity); //om inte funkar testa transform.position
+
     }
+
     void Update()
     {
         
