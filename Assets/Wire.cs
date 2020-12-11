69f4248684fb7822b10f22917f15723749c96042
Anton <anton.enell@wlwv.ntig.se>
2020-12-11 13:15:15 +0100

    wire tasken är nu en prefab

--- Assets/Wire.cs
+++ Assets/Wire.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Wire : MonoBehaviour
 {
+    Vector3 startpunkt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startpunkt = transform.parent.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDrag()
     {
-        
+        //musens position
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0;
+
+        //flytta sladden
+        transform.position = newPosition;
+
+        //ändra riktning på änden av sladden
+        Vector3 plats = newPosition - startpunkt;
+        transform.right = plats * transform.lossyScale.x;
     }
 }
