36cfcf732b199a22ad0c046ac9352c6346e70b70
Anton <anton.enell@wlwv.ntig.se>
2021-01-15 13:57:28 +0100

    freeze när man gör tasken

--- Assets/Scripts/Task.cs
+++ Assets/Scripts/Task.cs
@@ -7,7 +7,9 @@ public class Task : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject taskPrefab; //pls link me to my task, om inte funkar testa public GameObject taskPrefab = null; 
+    bool doingtask;
 
+    private Collider2D entryObject;
 
     void Start()
     {
@@ -20,12 +22,16 @@ public class Task : MonoBehaviour
     {
         
         Debug.Log("Task: ENTER from " + other.name);
+
+        entryObject = other;
+
         
     }
 
     private void OnTriggerExit2D(Collider2D other) // om man lämnar collidern stängs tasken
     {
         Debug.Log("Task: EXIT");
+        entryObject = null;
     }
 
     // Update is called once per frame
@@ -33,9 +39,28 @@ public class Task : MonoBehaviour
     {
         Debug.Log("Task: POPUP!");
 
-        GameObject.Instantiate(taskPrefab);
+        Instantiate(taskPrefab, Vector3.zero, Quaternion.identity); // för att spana in på specefik plats new Vector3.(-10,0,0)
 
-        Instantiate(taskPrefab, new Vector3(0, 0, -10), Quaternion.identity); //om inte funkar testa transform.position
+        //Check if player is doing task or not
+        if (!doingtask)
+        {
+            //Freeze the player position
+            
+            doingtask = true;
+            entryObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+        }
+        else if (doingtask)
+        {
+            //Unfreeze the player position
+            
+            doingtask = false;
+            entryObject.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+
+        //GameObject.Instantiate(taskPrefab);
+
+
+        
 
     }
 
