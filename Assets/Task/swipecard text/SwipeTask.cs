189fa2567732afa2c97a1eb92dd2677aa8ba6aa6
Anton <anton.enell@wlwv.ntig.se>
2021-01-29 21:36:51 +0100

    SwipeTask code

---
+++ Assets/Task/swipecard text/SwipeTask.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeTask : MonoBehaviour
+{
+    public List<SwipePoints>
+
+    internal void SwipePointTrigger(NewBehaviourScript newBehaviourScript)
+    {
+        throw new NotImplementedException();
+    }
+}
