922259e69b47e740813ad1bc635aa7158e9088af
Anton <anton.enell@wlwv.ntig.se>
2021-01-29 21:36:27 +0100

    Swipe card points code

---
+++ Assets/Task/swipecard text/SwipePoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private SwipeTask swipeTask;
+    private void Awake()
+    {
+        _swipeTask = GetComponentInParent<SwipeTask>();
+
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        _swipeTask.SwipePointTrigger(this);
+    }
+}
