ce0e7ef7b82a18c22558d9d49636eaa853276d36
Anton <anton.enell@wlwv.ntig.se>
2021-02-12 12:59:01 +0100

    Skrev kod för att klara eller fölora tasken

--- Assets/SwipeTask.cs
+++ Assets/SwipeTask.cs
@@ -9,6 +9,14 @@ public class SwipeTask : MonoBehaviour
 
     public float _countdownMax = 0.5f;
 
+    public GameObject _greenOn;
+
+    public GameObject _redOn;
+
+    public GameObject _completed;
+
+    public GameObject _tryagain;
+
     private int _currentSwipePointindex = 0;
 
     private float _cuntdown = 0;
@@ -20,8 +28,28 @@ public class SwipeTask : MonoBehaviour
         if (_currentSwipePointindex != 0 && _cuntdown <= 0)
         {
             _currentSwipePointindex = 0;
-            Debug.Log("nein error");
+            StartCoroutine(FinishTask(false));
         }
+    }
+
+    private IEnumerator FinishTask(bool wasSuccessful)
+    {
+        if (wasSuccessful)
+        {
+            _greenOn.SetActive(true);
+            _completed.SetActive(true);
+        }
+        else
+        {
+            _redOn.SetActive(true);
+            _tryagain.SetActive(true);
+        }
+
+        yield return new WaitForSeconds(1.5f);
+        _greenOn.SetActive(false);
+        _redOn.SetActive(false);
+        _completed.SetActive(false);
+        _tryagain.SetActive(false);
     }
 
     public void  SwipePointTrigger(SwipePoint swipePoint)
@@ -34,7 +62,7 @@ public class SwipeTask : MonoBehaviour
         if (_currentSwipePointindex >= _swipePoints.Count)
         {
             _currentSwipePointindex = 0;
-            Debug.Log("Yes nice");
+            StartCoroutine(FinishTask(true));
         }
     }
 
