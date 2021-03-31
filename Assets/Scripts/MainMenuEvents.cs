62c4c43e15681a8f2768124475526219e9d20a37
Leo <leo.hasselqvist@elev.ntig.se>
2021-04-01 00:13:00 +0200

    Lade till en välde basic meny

---
+++ Assets/Scripts/MainMenuEvents.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuEvents : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartLobby() //Switch scene to the lobby scene
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void JoinGame() //Switch scene to the input IP scene (or GameCode if we can write servers)
+    {
+        SceneManager.LoadScene("Lobby"); // TEMP, REPLACE WITH IP INPUT SCENE LATER
+    }
+
+    public void InitGame() // this starts the actual game like killing and shit.
+    {
+        SceneManager.LoadScene("MapSkeld");  // Start Skeld Map
+    }
+}
