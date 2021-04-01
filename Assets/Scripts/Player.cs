f36d46258b8cc48fea3626d2934ae616b0369b52
Leo <leo.hasselqvist@elev.ntig.se>
2021-04-01 18:10:15 +0200

    Tog bort parametrarna för vision, kill cooldown och kill distance.
    Lade in dessa i GameManager istället

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     string playerName;
-    int killDistance;
-    int killCooldown;
-    int visionMod;  
     bool isDead;
     bool isImposter;
     int emergencyMeetings;
@@ -16,12 +13,9 @@ public class Player : MonoBehaviour
 
     public Animator animator;
 
-    public Player(string playerName, int killDistance, int killCooldown, int visionMod, bool isDead, bool isImposter, int emergencyMeetings)
+    public Player(string playerName, bool isDead, bool isImposter, int emergencyMeetings)
     {
         this.playerName = playerName;
-        this.killDistance = killDistance;
-        this.killCooldown = killCooldown;
-        this.visionMod = visionMod;
         this.isDead = isDead;
         this.isImposter = isImposter;
         this.emergencyMeetings = emergencyMeetings;
