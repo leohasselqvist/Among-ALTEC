609bc8283c030ca71c007e2d39f684e179c6e067
Elias <elgu58hbg@gmail.com>
2021-05-21 13:20:15 +0200

    Förde över movement scripten till player script + fixade buggar

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float baseSpeed = 10;
+    [SerializeField]
+    private float speedMod = 1;
+
+    float horizontalMove = 0;
+    float verticalMove = 0;
+    float totalMove = 0;
+    float directionalMove = 0;
+
+    bool facingright = true;
+
+    public Animator animator;
+
+    private Collider2D selectedTask;
+
+    public Rigidbody2D rb;
+
     string playerName;
     bool isDead;
     bool isImposter;
@@ -11,8 +29,78 @@ public class Player : MonoBehaviour
 
     public GameObject playerPrefab;
 
-    public Animator animator;
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
 
+        Flip(horizontal);
+    }
+
+    void Update()
+    {
+        //Ta fram värden på spelarens rörelse
+        horizontalMove = Input.GetAxisRaw("Horizontal") * baseSpeed;
+        verticalMove = Input.GetAxisRaw("Vertical") * baseSpeed;
+
+        //Värde för spelarens rikting (vänster < 0 < höger)
+        directionalMove = Input.GetAxis("Horizontal");
+        Debug.Log("Direction value: " + directionalMove);
+
+        //Kolla om spelaren rör sig eller inte
+        totalMove = Mathf.Abs(horizontalMove) + Mathf.Abs(verticalMove);
+        animator.SetFloat("Speed", Mathf.Abs(totalMove));
+
+        var movement_h = Input.GetAxis("Horizontal") * baseSpeed * speedMod;
+        var movement_v = Input.GetAxis("Vertical") * baseSpeed * speedMod;
+        //transform.position += new Vector3(movement_h, movement_v, 0) * Time.deltaTime * baseSpeed * speedMod;*/
+
+        rb.velocity = new Vector2(movement_h, movement_v);
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            selectedTask.GetComponent<Task>().Popup();
+        }
+
+        if (false)
+        {
+            animator.SetBool("Dead", true);
+            this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Task: ENTER from " + other.name);
+        if (other.name == "Task")
+        {
+            selectedTask = other;
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Task: EXIT");
+        if (other.name == "Task")
+        {
+            selectedTask = null;
+        }
+    }
+
+    private void Flip(float horizontal)
+    {
+        if (horizontal > 0 && !facingright || horizontal < 0 && facingright)
+        {
+            facingright = !facingright;
+
+            Vector3 theScale = transform.localScale;
+
+            theScale.x *= -1;
+
+            transform.localScale = theScale;
+        }
+    }
     public Player(string playerName, bool isDead, bool isImposter, int emergencyMeetings)
     {
         this.playerName = playerName;
@@ -30,17 +118,6 @@ public class Player : MonoBehaviour
     {
         GameObject a = Instantiate(playerPrefab) as GameObject;
         a.transform.position = this.transform.position;
-
-    }
-
-    void Update()
-    {
-
-        if (isDead == true)
-        {
-            animator.SetBool("Dead", true);
-            this.GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezePosition;
-        }
 
     }
 }
