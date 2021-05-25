1dbdaf454cd57e938c48b04e3b0010539a488b11
Leo <leo.hasselqvist@elev.ntig.se>
2021-05-25 10:22:26 +0200

    Uppdaterade create lobby systemet

---
+++ Assets/Scripts/CreateLobby.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreateLobby : Button
+{
+    private string username;
+    // Start is called before the first frame update
+    public override void OnPointerClick(PointerEventData eventData)
+    {
+        base.OnPointerClick(eventData);
+        username = transform.parent.Find("InputField").GetComponent<InputField>().text;
+        PersonalSettings.Instance.username = username;
+        SceneManager.LoadScene("Lobby");
+    }
+}
