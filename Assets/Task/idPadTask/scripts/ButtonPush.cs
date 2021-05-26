772531631b976176fb09f2caea31eea57cac747b
Anton <anton.enell@wlwv.ntig.se>
2021-05-26 22:58:53 +0200

    buttonPush script

---
+++ Assets/Task/idPadTask/scripts/ButtonPush.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class ButtonPush : MonoBehaviour
+{
+    public static event Action<string> ButtonPressed = delegate { };
+    private int devider;
+    private string buttonName, buttonValue;
+    void Start()
+    {
+        buttonName = gameObject.name;
+        devider = buttonName.IndexOf("_");
+        buttonValue = buttonName.Substring(0, devider);
+
+        gameObject.GetComponent<Button>().onClick.AddListener(ButtonClickted);
+    }
+
+    private void ButtonClickted()
+    {
+        ButtonPressed(buttonValue);
+    }
+}
