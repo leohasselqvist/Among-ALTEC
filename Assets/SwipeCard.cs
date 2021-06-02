37be12cb6abaed602f80632649a3a7d30a16cab0
Anton <anton.enell@wlwv.ntig.se>
2021-06-02 19:48:35 +0200

    kommenterat

--- Assets/SwipeCard.cs
+++ Assets/SwipeCard.cs
@@ -5,29 +5,29 @@ using UnityEngine.EventSystems;
 
 public class SwipeCard : MonoBehaviour
 {
-    private Vector3 mousePosition;
-    private Rigidbody2D rb;
-    private Vector2 direction;
-    private float moveSpeed = 800f;
+    private Vector3 mousePosition; // position av musen
+    private Rigidbody2D rb; //ridget body
+    private Vector2 direction; //riktning
+    private float moveSpeed = 800f; //hastighet
 
     
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); //får in ridgetbody comoneten som kopplas på kortet
     }
 
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0)) //ifall man trycker innfaör ridgetbody med vänste musknapp
         {
-            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); //vet faktiskt inte rikitgt vad camera grejen gör, blev tipsad från internet
             direction = (mousePosition - transform.position).normalized;
-            rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed);
+            rb.velocity = new Vector2(direction.x * moveSpeed, direction.y * moveSpeed); //hastighten är basicly x och y kordinaten multiplicerat med hastighten
         }
         else
         {
-            rb.velocity = Vector2.zero;
+            rb.velocity = Vector2.zero; //anars så fryser kortet på plats
         }
     }
 
