6682e89e62a41ba6a11e911ca8613979696d5a82
Anton <anton.enell@wlwv.ntig.se>
2021-06-02 19:49:05 +0200

    kommenterat

--- Assets/Task/Switch.cs
+++ Assets/Task/Switch.cs
@@ -4,36 +4,36 @@ using UnityEngine;
 
 public class Switch : MonoBehaviour
 {
-    public GameObject up;
-    public GameObject on;
-    public bool isOn;
-    public bool isUp;
+    public GameObject up; // kopplas till prefaben av nakppen uppot
+    public GameObject on; // kopplas till prefaben av lampan på
+    public bool isOn; // Boolen som kontrolerar lampan
+    public bool isUp; // Boolen som kontrolerar kanppen
     // Start is called before the first frame update
     void Start()
     {
-        on.SetActive(isOn);
-        up.SetActive(isUp);
+        on.SetActive(isOn); //för att senare kunna ge olika kanppar ett upp elkler ner värde
+        up.SetActive(isUp); //same
 
-        if (isOn)
+        if (isOn) // ifall lamporna är igång
         {
-            Main.Instance.SwitchChange(1);
+            Main.Instance.SwitchChange(1); // räkna switchen som är igång, och kallar på Main variablen Instance SwitchChange
         }
     }
-    private void OnMouseUp()
+    private void OnMouseUp() //triggar när musen rör collidern
     {
-        isUp = !isUp;
-        isOn = !isOn;
+        isUp = !isUp; //för att invertera så att om den är false blir den true
+        isOn = !isOn; //same
 
-        on.SetActive(isOn);
-        up.SetActive(isUp);
+        on.SetActive(isOn); //om ison triggas så sätts on aktit
+        up.SetActive(isUp); //same
 
-        if (isOn)
+        if (isOn) // om lamporna är på
         {
-            Main.Instance.SwitchChange(1);
+            Main.Instance.SwitchChange(1); // plussar på ett om den är igång
         }
         else
         {
-            Main.Instance.SwitchChange(-1);
+            Main.Instance.SwitchChange(-1); // subtaherar 1 om den är av
         }
     }
 }
