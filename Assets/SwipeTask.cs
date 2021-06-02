0d2b961dceccd97b34249ebc97cf1cfd625a1c2d
Anton <anton.enell@wlwv.ntig.se>
2021-06-02 19:48:45 +0200

    kommenterat

--- Assets/SwipeTask.cs
+++ Assets/SwipeTask.cs
@@ -5,78 +5,80 @@ using UnityEngine;
 
 public class SwipeTask : MonoBehaviour
 {
-    public List<SwipePoint> _swipePoints = new List<SwipePoint>();
+    public List<SwipePoint> _swipePoints = new List<SwipePoint>(); //sparar swipepoinsen i en lista
 
-    public float _countdownMax = 0.5f;
+    public float _countdownMax = 0.5f; //timer som andvänds för att se till så att tiden mellan colidersan inte översiger
 
-    public GameObject _greenOn;
+    public GameObject _greenOn; //lampa på
 
-    public GameObject _redOn;
+    public GameObject _redOn; // lampa av
 
-    public GameObject _completed;
+    public GameObject _completed; //completed text
 
-    public GameObject _tryagain;
+    public GameObject _tryagain; //try agin text
 
-    private int _currentSwipePointindex = 0;
+    private int _currentSwipePointindex = 0; //vilken swipepoint som kotet rör
 
-    private float _cuntdown = 0;
+    private float _cuntdown = 0; //cuntddown når sitt slut
 
-    public GameObject winText;
+    public GameObject winText; // wintexten
 
     private void Update()
     {
-        _cuntdown -= Time.deltaTime;
+        _cuntdown -= Time.deltaTime; //subtraherar tiden som går i cuntdownen
 
-        if (_currentSwipePointindex != 0 && _cuntdown <= 0)
+        if (_currentSwipePointindex != 0 && _cuntdown <= 0) //ifall cuntdowen når noll eller den inte når alla swipepoint index
         {
-            _currentSwipePointindex = 0;
-            StartCoroutine(FinishTask(false));
+            _currentSwipePointindex = 0; 
+            StartCoroutine(FinishTask(false)); //starta finishTask False
         }
     }
 
-    private IEnumerator FinishTask(bool wasSuccessful)
+    public void SwipePointTrigger(SwipePoint swipePoint) // funktionen kallas av varje swipepoint
+    {
+        if (swipePoint == _swipePoints[_currentSwipePointindex]) //ifall swipepointen faller i årdning
+        {
+            _currentSwipePointindex++; //plussa på 1 på swipepontindexet
+            _cuntdown = _countdownMax; //cuntdown är då lika med den tid som ska vara max imellan swipepointsen
+        }
+        if (_currentSwipePointindex >= _swipePoints.Count) //ifall man når den sista swipepointen
+        {
+            _currentSwipePointindex = 0;
+            StartCoroutine(FinishTask(true)); //starta finishtask true
+
+        }
+    }
+
+    private IEnumerator FinishTask(bool wasSuccessful) //startar om den kallas i koden
     {
         if (wasSuccessful)
         {
-            _greenOn.SetActive(true);
-            _completed.SetActive(true);
-            winText.SetActive(true);
-            StartCoroutine(Timer());
+            _greenOn.SetActive(true); //grön knapp
+            _completed.SetActive(true); //texten korect
+            winText.SetActive(true); //win text
+            StartCoroutine(Timer()); // starta timer
         }
         else
         {
-            _redOn.SetActive(true);
-            _tryagain.SetActive(true);
+            _redOn.SetActive(true); //rödlampa
+            _tryagain.SetActive(true); //texten försök igen
         }
 
-        yield return new WaitForSeconds(1.5f);
-        _greenOn.SetActive(false);
+        yield return new WaitForSeconds(1.5f); //leverear en i tagen 
+        _greenOn.SetActive(false); //visa ingen 
         _redOn.SetActive(false);
         _completed.SetActive(false);
         _tryagain.SetActive(false);
     }
 
-    private IEnumerator Timer()
+    private IEnumerator Timer() //startar vid kallelse
     {
-        yield return new WaitForSeconds(2);
-        Destroy(gameObject);
-        Destroy(winText);
+        yield return new WaitForSeconds(2); //vänta 
+        Destroy(gameObject); //förstör
+        Destroy(winText); //förstör
     }
 
-    public void  SwipePointTrigger(SwipePoint swipePoint)
-    {
-        if (swipePoint == _swipePoints[_currentSwipePointindex])
-        {
-            _currentSwipePointindex++;
-            _cuntdown = _countdownMax;
-        }
-        if (_currentSwipePointindex >= _swipePoints.Count)
-        {
-            _currentSwipePointindex = 0;
-            StartCoroutine(FinishTask(true));
 
-        }
-    }
 
     
 }
